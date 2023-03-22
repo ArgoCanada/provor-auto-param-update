e395eeda052193affe67d7d53b19a10ed2ed3f0a
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2023-03-22 09:54:30 -0300

    time list, updated ways to make changes

--- update-params.py
+++ update-params.py
@@ -6,6 +6,9 @@ from io import BytesIO
 import pandas as pd
 import provorpy as ppy
 
+update_from_command_file = True
+update_from_user_list = False
+
 #------------------------------------------------------------------------------
 # connect to DFO FTP server
 #------------------------------------------------------------------------------
@@ -24,18 +27,20 @@ for imei in imei_numbers:
 
     # get most recent profile time for each float
     last_profile_time = ppy.file_time(ftp.nlst(f'{imei}/*.bin'))
+    print(imei, last_profile_time)
     # if the timing window is implemented properly, this is redundant, but essentially if there
     # already exists a command file, we don't need to upload a new one so exclude that float
     # this will also prevent overriding command files that may have been uploaded manually
-    no_command_file_exists = f'{imei}/RUDICS_cmd.txt' not in ftp.nlst(f'{imei}/')
+    no_command_file_exists = f'{imei}/remote/RUDICS_cmd.txt' not in ftp.nlst(f'{imei}/remote/*')
     within_last_day = ct - last_profile_time < pd.Timedelta(hours=25*7)
     last_command_time = ppy.file_time(ftp.nlst(f'{imei}/*cmd.txt'))
     recent_command = abs(last_command_time - last_profile_time) < pd.Timedelta(hours=12)
     param_update = no_command_file_exists and within_last_day and recent_command
 
-    print(imei, last_profile_time)
 
     if param_update:
+
+        print(f'Updating {imei}...')
 
         # check time of command file - make sure it corresponds to this profile
         # effectively this means that the process must be manually started with 
@@ -47,15 +52,24 @@ for imei in imei_numbers:
         df = ppy.read_cmd_response(r)
 
         if 'PM 4' in df.index:
-            new_time = df.Value.loc['PM 4'] - 5
-            new_time = new_time + 24 if new_time < 0 else new_time
+            if update_from_command_file:
+                new_time = df.Value.loc['PM 4'] - 5
+                new_time = new_time + 24 if new_time < 0 else new_time
+            elif update_from_user_list:
+                df = pd.read_csv('time_list.csv')
+                new_time = df.param.iloc[0]
+                df.iloc[:-1] = df.iloc[1:]
+                df.iloc[-1] = new_time
+                df.to_csv('time_list.csv')
+            else:
+                raise ValueError('No source for new time selected')
         
             filename = f'commands/{ct.year}{ct.month}{ct.day}_{imei}_auto_time_update_cmd.txt'
             with open(filename, 'w') as f:
                 f.write(f'!PM 4 {new_time:d}\r\n')
             
             with open(filename, 'rb') as f:
-                ftp.storbinary(f'STOR {imei}/RUDICS_cmd.txt', f)
+                ftp.storbinary(f'STOR {imei}/remote/RUDICS_cmd.txt', f)
             
             with open(logfile, 'a') as f:
-                f.write(f'\n[{ct.year}-{ct.month}-{ct.day}] Updated {imei} surfacing time from {df.Value.loc["PM 4"]} to {new_time}')
+                f.write(f'\n[{ct.year:04d}-{ct.month:02d}-{ct.day:02d}] Updated {imei} surfacing time from {df.Value.loc["PM 4"]} to {new_time}')
